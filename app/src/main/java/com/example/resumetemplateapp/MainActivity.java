6ae7c0cec3feb45a7af1ceb9cc938cce5614a948
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-02-10 21:28:37 +0500

    Keys added + Other in progress

--- app/src/main/java/com/example/resumetemplateapp/MainActivity.java
+++ app/src/main/java/com/example/resumetemplateapp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.resumetemplateapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -27,6 +34,18 @@ public class MainActivity extends AppCompatActivity {
     ImageButton addressEdit;
 
     // Professional Detail Section
+    TextView objectives;
+    ConstraintLayout skills;
+    ConstraintLayout projects;
+    ConstraintLayout activities;
+
+    ImageButton objectivesEdit;
+    ImageButton skillsEdit;
+    ImageButton projectsEdit;
+    ImageButton activitiesEdit;
+
+
+    ActivityResultLauncher<Intent> launcher;
 
 
     @Override
@@ -39,5 +58,87 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
+
+        // Personal Info
+        profilePhoto.setOnClickListener((v) -> {
+            Intent i = new Intent(Intent.ACTION_PICK);
+            i.setType("image/*");
+            startActivity(i);
+        });
+
+        email.setOnClickListener((v) -> {
+            String userEmail = v.getText().toString();
+
+            if (!userEmail.isEmpty()) {
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("mailto:" + userEmail));
+                startActivity(i);
+            }
+            else{
+                Toast.makeText(MainActivity.this, "Email not Found", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ph_no.setOnClickListener((v) -> {
+            String userPh_no = v.getText().toString();
+
+            if(!userPh_no.isEmpty())
+            {
+                Intent i = new Intent(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:" + userPh_no));
+                startActivity(i);
+            }
+            else
+            {
+                Toast.makeText(this, "Phone Number Not Found", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        address.setOnClickListener((v) -> {
+            String userAddress = v.getText().toString();
+
+            if(!userAddress.isEmpty())
+            {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("google.navigation:q=" + userAddress));
+                startActivity(i);
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this, "Address Not Found", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        nameEdit.setOnClickListener((v) -> {
+            nameEdit
+        });
+
+    }
+
+    private void init(){
+        // Personal Info
+        profilePhoto = findViewById(R.id.profile_photo);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        ph_no = findViewById(R.id.ph_no);
+        address = findViewById(R.id.address);
+
+        nameEdit = findViewById(R.id.name_field_edit);
+        emailEdit = findViewById(R.id.email_field_edit);
+        ph_noEdit = findViewById(R.id.ph_no_field_edit);
+        addressEdit = findViewById(R.id.address_field_edit);
+
+        // Professional Info
+        objectives = findViewById(R.id.objectives_text);
+        skills = findViewById(R.id.skills_text);
+        projects = findViewById(R.id.projects_text);
+        activities = findViewById(R.id.activities_text);
+
+        objectivesEdit = findViewById(R.id.objectives_field_edit);
+        skillsEdit = findViewById(R.id.skills_field_edit);
+        projectsEdit = findViewById(R.id.projects_field_edit);
+        activitiesEdit = findViewById(R.id.activities_field_edit);
     }
 }
---
+++ app/src/main/java/com/example/resumetemplateapp/keys.java
@@ -0,0 +1,13 @@
+package com.example.resumetemplateapp;
+
+public class keys {
+    public static final String name_key = "Name";
+    public static  final  String email_key = "Email";
+    public static final String ph_no_key = "Phone Number";
+    public static final String address_key = "Address";
+    public static final String objective_key = "Objectives";
+    public static final String skills_key = "Skills";
+    public static final String projects_key = "Projects";
+    public static final String activities_key = "Activities";
+    public static final String universal_key = "Universal";
+}
